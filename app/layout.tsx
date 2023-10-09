4411268d8cb9d31095e342cf89c474caa5c58872
priyanshi <priyanshikhippal6482@gmail.com>
2023-10-09 20:29:05 +0530

    udated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-
-
 import { ClerkProvider } from '@clerk/nextjs'
- 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+import type { Metadata } from "next"; 
+import {Inter,Space_Grotesk} from "next/font/google"
+import './globals.css';
+
+const inter=Inter({
+  subsets:['latin'],
+  weight:['100','200','300','400','500','600','700'],
+  variable:"--font-inter"
+} 
+)
+
+const spaceGrotesk=Space_Grotesk({
+  subsets:['latin'],
+  weight:['300','400','500','600','700'],
+  variable:"--font-spaceGrotesk"
+} 
+)
+
+export const metadata:Metadata = {
+  title: 'DevCosmos',
+  description:'A community-driven platform for asking and answering programming question . Get help ,share Knowledge , and collaborate with developers from around the world. ',
+  icons :{
+    icon:'assets/images/site-logo.svg'
+  }
 }
  
 export default function RootLayout({
@@ -13,9 +32,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={
+      {
+        elements:{
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink:
+          'primary-text-gradient hover:text-primary-500'
+        }
+      }
+    }
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
