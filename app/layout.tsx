aafb4e5b7b4bd61c428a32eebd2c38e92e1935c2
priyanshi <priyanshikhippal6482@gmail.com>
2023-10-09 23:07:54 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,43 @@
 import React from "react";
-import { ClerkProvider } from '@clerk/nextjs'
-import type { Metadata } from "next"; 
-import {Inter,Space_Grotesk} from "next/font/google"
-import './globals.css';
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
 
-const inter=Inter({
-  subsets:['latin'],
-  weight:['100','200','300','400','500','600','700'],
-  variable:"--font-inter"
-} 
-)
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
 
-const spaceGrotesk=Space_Grotesk({
-  subsets:['latin'],
-  weight:['300','400','500','600','700'],
-  variable:"--font-spaceGrotesk"
-} 
-)
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
-export const metadata:Metadata = {
-  title: 'DevCosmos',
-  description:'A community-driven platform for asking and answering programming question . Get help ,share Knowledge , and collaborate with developers from around the world. ',
-  icons :{
-    icon:'assets/images/site-logo.svg'
-  }
-}
- 
+export const metadata: Metadata = {
+  title: "DevCosmos",
+  description:
+    'A community-driven platform for asking and answering programming question . Get help ,share Knowledge , and collaborate with developers from around the world. ',
+    icons: {
+      icon: '/assets/images/site-logo.svg'
+    }
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider
-    appearance={
-      {
-        elements:{
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink:
-          'primary-text-gradient hover:text-primary-500'
-        }
-      }
-    }
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
     >
       <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
@@ -49,5 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
